16971550c9544c9c4114346d12b3f47c3e1f7a9b
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-04-30 21:46:15 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240430/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -0,0 +1,38 @@
+"""
+n, n-1, n-1, n-2, n-2 n-3 n-3/ n-4,,...
+n / n = n-1 * 2 ... /  
+"""
+
+ORIENTS = {
+    0 : (  0,  1 ), 
+    1 : (  1,  0 ), 
+    2 : (  0, -1 ),
+    3 : ( -1,  0 ), 
+}
+
+n, m = map(int,input().split())
+grp = [[0]*m for _ in range(n)]
+
+xx, yy, direct, curr_num = 0, -1, 0, 1
+
+while (curr_num <= n*m):
+    dx, dy = ORIENTS[direct]
+    nx, ny = xx + dx, yy + dy
+
+    if not (
+        0<= nx < m and
+        0<= ny < n and
+        grp[nx][ny] == 0 
+    ) :
+        direct = (direct + 1) % 4
+        continue
+    
+    xx, yy = nx, ny
+    
+    grp[xx][yy] = curr_num
+    curr_num += 1
+
+for row in grp:
+    for el in row:
+        print(el, end=" ")
+    print()
