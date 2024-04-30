1578d5c7e23e206ee595d9e3bedc6ad6f7fd1189
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-04-30 19:56:13 +0900

    Title: 문자에 따른 명령 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/문자에 따른 명령 2/text-based-commands2.py
+++ 240430/문자에 따른 명령 2/text-based-commands2.py
@@ -15,8 +15,8 @@ ORIENTS = {
     3: [-1,  0],
 }
 
-curr_orient = 0
-curr_x, curr_y = 0, 0
+curr_orient, curr_x, curr_y = 0, 0, 0
+
 for commend in str(input()):
     if commend == "L":
         curr_orient = (curr_orient - 1 + 4) % 4
@@ -26,4 +26,5 @@ for commend in str(input()):
         dx,dy = ORIENTS[curr_orient]
         curr_x += dx
         curr_y += dy
+
 print(curr_x, curr_y)
