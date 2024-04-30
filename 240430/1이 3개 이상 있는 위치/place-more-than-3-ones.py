cb2041ce22ee833688c645399158b9984a97b68d
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-04-30 20:35:38 +0900

    Title: 1이 3개 이상 있는 위치, Time: 172ms, Memory: 26MB, Status: Passed - CodeTree

--- 240430/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240430/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -3,27 +3,20 @@ grp = [list(map(int,input().split())) for _ in range(size)]
 rst_counter = 0
 
 moves = [
-    [ 1,  0], 
-    [ 0,  1], 
-    [-1,  0],
-    [ 0, -1]
+    ( 1,  0), 
+    ( 0,  1), 
+    (-1,  0),
+    ( 0, -1)
 ] 
 
 for idx in range(size):
     for jdx in range(size):
-        tmp_counter = 0
-        for dx, dy in moves:
-            nx = idx + dx
-            ny = jdx + dy
-            
-            if (nx >= size or nx < 0 ) or (ny >= size or ny < 0):
-                continue
-
-            if grp[nx][ny] == 1:
-                tmp_counter += 1 
-
-            if tmp_counter >= 3:
-                rst_counter += 1
-                break
+        
+        rst_counter += 1 if sum(
+            0 <= idx + dx < size 
+            and  0 <= jdx + dy < size
+            and  grp[idx + dx][jdx + dy] == 1
+            for dx, dy in moves
+        ) >= 3 else 0
 
 print(rst_counter)
