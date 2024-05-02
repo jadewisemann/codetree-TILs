cb41777295f07b307193e15875374e53c4f4416e
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-02 23:30:26 +0900

    Title: 특정 구간의 합, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/특정 구간의 합/sum-of-specific-intervals.py
+++ 240502/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,23 +1,13 @@
 I = lambda: map(int, input().split())
 
-
 n, m =  I()
-
-global memo_sums
-memo_sums = [0] * (n+1)
-
-def partial_sum (a,b):
-    if not memo_sums[b]:
-        for idx in range(1, b+1):
-            if not memo_sums[idx]:
-                memo_sums[idx] = memo_sums[idx-1] + sequenc_A[idx]
-    return(memo_sums[b] - memo_sums[a-1] )
-
-
 
 global sequenc_A
 sequenc_A = [0] + list(I())
 
+for idx in range(1, n+1):
+    sequenc_A[idx] += sequenc_A[idx-1]
+
 for _ in range(m):
     a1, a2 = I()
-    print(partial_sum(a1, a2))
+    print(sequenc_A[a2] - sequenc_A[a1-1])
