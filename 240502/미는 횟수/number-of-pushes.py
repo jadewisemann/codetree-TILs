21cc5b3b6f550e482ee2dc01615aeaab90b94a96
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-02 22:15:02 +0900

    Title: 미는 횟수, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240502/미는 횟수/number-of-pushes.py
+++ 240502/미는 횟수/number-of-pushes.py
@@ -2,8 +2,8 @@ I = lambda: str(input())
 
 s1, s2 = I(), I()
 
-counter = 0
-while not(s1 == s2):
-    s1 = s1[-1] + s1[:-1]
-    counter += 1
-print(counter)
+junctions = [idx for idx, char in enumerate(s2) if char == (first_char:=s1[0])] 
+
+for juncion in junctions:
+    if s1[juncion:] + s1[:juncion]  == s2:
+        print(len(s1) - juncion)
