db45a34f52ba9e76b4bcf977999a69d221dedfc9
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-02 21:36:44 +0900

    Title: 규칙에 따라 밀기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/규칙에 따라 밀기/push-by-the-rules.py
+++ 240502/규칙에 따라 밀기/push-by-the-rules.py
@@ -2,12 +2,10 @@ I = lambda: str(input())
 
 org = I()
 
-head = 0
-
 for commend in I():
-    if commend == "L":
-        head += 1
-    elif commend == "R":
-        head -= 1
-
-print(org[head:] + org[0:head])
+    if commend == "R": 
+        org = org[-1] + org[:-1] 
+    elif commend == "L":
+        org = org[1:] + org[0]
+        
+print(org)
