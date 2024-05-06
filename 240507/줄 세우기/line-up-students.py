44f8726deb8df360fe347ea2ac3b03b21d65e861
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-07 01:33:12 +0900

    Title: 줄 세우기, Time: 180ms, Memory: 25MB, Status: Passed - CodeTree

--- 240507/줄 세우기/line-up-students.py
+++ 240507/줄 세우기/line-up-students.py
@@ -1,16 +1,15 @@
 class Student:
 
     def __init__(self, height, weight, idx):
-        self.height = height
-        self.weight = weight
-        self.idx = idx + 1
+        self.height = int(height)
+        self.weight = int(weight)
+        self.index = idx + 1
         
     
     def __repr__(self):
-        return f"{self.height} {self.weight} {self.idx}"
+        return f"{self.height} {self.weight} {self.index}"
     
 for student in sorted(
     [Student(*input().split(), idx) for idx in range(int(input()))],
-    key= lambda x: (x.height, x.weight, x.idx),
-    reverse = True
+    key= lambda x: (-x.height, -x.weight, x.index),
 ): print(student)
