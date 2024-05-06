03321e8942f9389c1a6728e78838727f03fa8708
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-07 00:26:05 +0900

    Title: 폭탄 해제, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/폭탄 해제/bomb-removal.py
+++ 240507/폭탄 해제/bomb-removal.py
@@ -3,11 +3,10 @@ class Bomb:
         self.code = code
         self.color = color
         self.time = time
-    
+
     def __repr__(self):
         return(
             f"code : {self.code}\ncolor : {self.color}\nsecond : {self.time}"
         )
     
-bomb = Bomb(*input().split())
-print(bomb)
+print(bomb:=Bomb(*input().split()))
