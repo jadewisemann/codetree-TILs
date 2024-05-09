9e60b167f497cb33aae9c507205906c5ff46d5cc
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-10 04:26:52 +0900

    Title: 흰검 칠하기, Time: 211ms, Memory: 28MB, Status: Passed - CodeTree

--- 240510/흰검 칠하기/painting-white-black.py
+++ 240510/흰검 칠하기/painting-white-black.py
@@ -10,22 +10,22 @@ for _ in range(int(input())):
 
 	if dirc == "R":
 		for idx in range(val):
-			c_idx = cur + idx + OFFSET
-			if not arr[c_idx].endswith("B"):
-				arr[c_idx] += "B"
+			arr[cur + idx + OFFSET] += "B"
 		cur += val - 1
 
 	elif dirc == "L":
 		for idx in range(val):
-			c_idx = cur - idx + OFFSET
-			if not arr[c_idx].endswith("W"):
-				arr[c_idx] += "W"
+			arr[cur - idx + OFFSET] += "W"
 		cur -= val - 1
 
 
 bb, ww, gg = 0 , 0 , 0 
 for el in filter(lambda el: el !="" ,arr):
-	if len(el) >= 4:
+	if (
+		len(el) >= 4 
+		and el.count("B") >= 2 
+		and el.count("W") >= 2
+	): 
 		gg += 1
 	else:
 		if el.endswith("B"):
