26dbed31b8cb79b4f2132a94f4ac37b73b397998
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-10 01:48:14 +0900

    Title: 왔다 갔던 구역 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240510/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240510/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,25 +1,13 @@
-result = 0
-arr = [0] * (2 * 100 * 10 + 1)
-cur = 1000
-min_cur = 2001
-max_cur = 0
+vis = {}
+cur = 0
 
 for _ in range(int(input())):
-
-    max_cur = max(max_cur, cur)
-    min_cur = min(min_cur, cur)
-
     val, direct = input().split()
-    side = 1 if direct == "R" else -1
-    
     for _ in range(int(val)):
-        cur += side 
-        arr[cur] += 1
+        cur += (side:=1 if direct == "R" else -1)
+        if cur in vis: 
+            vis[cur] += 1
+        else: 
+            vis[cur] = 1
 
-counter = 0
-
-for idx in range(min_cur, max_cur+1):
-    if arr[idx] >= 2 : 
-        counter += 1
-
-print(counter)
+print(sum( 1 for value in vis.values() if value >= 2))
