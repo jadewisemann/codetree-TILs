78f08a6d3a8d73f304efb938982828fff6f5d6ce
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-10 21:12:57 +0900

    Title: 최대 증가 부분 수열, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240510/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240510/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,6 +1,6 @@
-dp = [0] * (N:= int(input()))
+dp = [1] * (N:= int(input()))
 arr = list(map(int,input().split()))
 
 for idx in range(1, N):
-    dp[idx] = max(([dp[jdx] for jdx in filter(lambda jdx: arr[jdx] <= arr[idx] ,[jdx for jdx in range(idx)])])) + 1
-print (max(dp)+1)
+    dp[idx] = max([dp[jdx] for jdx in range(idx) if arr[jdx] <= arr[idx]], default=0) + 1
+print (max(dp))
