79634b2d796e8992a691cb336f9ebbb4324041e1
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-05-10 19:39:22 +0900

    Title: 피보나치 수, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240510/피보나치 수/fibonacci-number.py
+++ 240510/피보나치 수/fibonacci-number.py
@@ -1,10 +1,16 @@
-# tabulation
+#  memoization
 
-fibo =  [0,1,1]
+memo ={
+    0: 0,
+    1: 1,
+    2: 1
+}
 
-N = int(input())
 
-for _ in range(3, N+1):
-    fibo.append(fibo[-1] + fibo[-2])
 
-print(fibo[N])
+def fibo(n):
+    if n  not in memo:
+        memo[n] = fibo(n - 1) + fibo (n -2)
+    return memo[n]
+ 
+print(fibo(int(input())))
