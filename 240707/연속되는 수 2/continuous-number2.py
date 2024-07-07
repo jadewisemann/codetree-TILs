a12b8184f49186609d35ffaf0e515c956a527a20
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-07-07 15:16:15 +0900

    Title: 연속되는 수 2, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240707/연속되는 수 2/continuous-number2.py
+++ 240707/연속되는 수 2/continuous-number2.py
@@ -1,14 +1,32 @@
+# result = 0
+# last = 0
+# local= 1
+# for _ in range(int(input())):
+#     curr = int(input())
+#     if curr == last :
+#         local += 1
+#     else:
+#         result = max(local, result)
+#         local = 1
+
+#     last = curr
+
+# print(result)
+
 result = 0
-last = 0
-local= 1
+last = None
+local = 0
+
 for _ in range(int(input())):
     curr = int(input())
-    if curr == last :
+    if curr == last:
         local += 1
     else:
         result = max(local, result)
-        local = 1
-
+        local = 1  # 새로 시작할 때 1로 시작해야 합니다.
+    
     last = curr
+
+result = max(local, result)  # 마지막 연속된 숫자의 길이를 반영
 
 print(result)
