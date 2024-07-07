c3299419a9696540d00c4a6af0679fa81e199c18
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-07-07 15:22:56 +0900

    Title: 연속되는 수 4, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240707/연속되는 수 4/continuous-number4.py
+++ 240707/연속되는 수 4/continuous-number4.py
@@ -1,17 +1,22 @@
-result = 0
-last = 0
-local= 1
-for _ in range(int(input())):
-    curr = int(input())
-    if curr > last :
-        local += 1
-    else:
-        result = max(local, result)
-        local = 1
+def max_increasing_subsequence_length(n, sequence):
+    if n == 0:
+        return 0
 
-    last = curr
-    
-result = max(local, result)
+    max_length = 1
+    current_length = 1
 
+    for i in range(1, n):
+        if sequence[i] > sequence[i - 1]:
+            current_length += 1
+            max_length = max(max_length, current_length)
+        else:
+            current_length = 1
 
-print(result)
+    return max_length
+
+# 입력
+n = int(input())
+sequence = [int(input()) for _ in range(n)]
+
+# 최대 길이 계산 및 출력
+print(max_increasing_subsequence_length(n, sequence))
