8e1b6fcdeb783f3bdb0727dfaa407e9c2d338346
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-07-07 15:19:43 +0900

    Title: 연속되는 수 4, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240707/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,17 @@
+result = 0
+last = 0
+local= 1
+for _ in range(int(input())):
+    curr = int(input())
+    if curr > last :
+        local += 1
+    else:
+        result = max(local, result)
+        local = 1
+
+    last = curr
+    
+result = max(local, result)
+
+
+print(result)
