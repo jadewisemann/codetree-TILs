1c76999a480a61a4c1648121b530491b05006d8b
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-07-09 17:39:33 +0900

    Title: 벌금은 누구에게, Time: 128ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240709/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,16 @@
+N, M, K = tuple(map(int, input().split()))
+
+students = [idx +  1 for idx in range(N)]
+penalties = [K for _ in range(N)]
+rst = -1
+
+for _ in range(M):
+    num = int(input())
+    
+    if penalties[ num - 1 ] == 1:
+        rst = num
+        break
+
+    penalties[num - 1] -= 1
+
+print(num)
