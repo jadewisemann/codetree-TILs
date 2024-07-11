1bc919e62158138a81bd934a3c2d45d480158081
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-07-11 16:00:50 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240711/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,47 @@
+MOVE = [ [0, 1], [1, 0], [0, -1], [-1, 0] ]
+
+n, m = map(int,input().split())
+grp = [[0]*m for _ in range(n)]
+
+col_counter, low_counter = n, m-1
+xx, yy = 0, -1
+char = ord("A")
+move_idx = 0
+
+
+while True:
+    
+    if col_counter == 0: break
+
+    for _ in range(col_counter):
+        dx, dy = MOVE[move_idx]
+        xx += dx
+        yy += dy
+        grp[xx][yy] = chr(char)
+
+        char += 1
+
+    col_counter -= 1
+
+    move_idx = (move_idx + 1)  % 4
+    
+    if low_counter == 0: break
+
+    for _ in range(low_counter):
+
+        dx, dy = MOVE[move_idx]
+        xx += dx
+        yy += dy
+
+        grp[xx][yy] = chr(char)
+        char += 1
+
+    low_counter -=  1
+
+    move_idx = (move_idx + 1)  % 4
+
+
+for row in grp:
+    for num in row:
+        print(num, end=" ")
+    print()
