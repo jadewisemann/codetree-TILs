7e141b2176a8ea5356f79c92d0922cb62df331db
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-07-11 17:23:49 +0900

    Title: 최고의 13위치, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240711/최고의 13위치/best-place-of-13.py
+++ 240711/최고의 13위치/best-place-of-13.py
@@ -1,11 +1,7 @@
 n = int(input())
-grp = [
-    list(map(int, input().split())) for _ in range(n)
-]
-
 rst = 0
 
-for row in grp:
+for row in [ list(map(int, input().split())) for _ in range(n) ]:
     for idx in range(0,n-2):
         rst = max(rst, row[idx] + row[idx+1] + row[idx+2])
         
