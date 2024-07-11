0104d21ce7d757061b702bc210f61151ff6ff12e
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-07-11 15:50:55 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,43 @@
+MOVE = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+n, m = map(int,input().split())
+grp = [[0]*m for _ in range(n)]
+
+col_counter, low_counter = n, m-1
+xx, yy = -1, 0 
+number = 1
+move_idx = 0
+
+
+while col_counter >= 0 and low_counter >= 0:
+    
+    for _ in range(col_counter):
+        dx, dy = MOVE[move_idx]
+        xx += dx
+        yy += dy
+        grp[xx][yy] = number
+
+        number += 1
+
+    col_counter -= 1
+
+    move_idx = (move_idx + 1)  % 4
+    
+    for _ in range(low_counter):
+
+        dx, dy = MOVE[move_idx]
+        xx += dx
+        yy += dy
+
+        grp[xx][yy] = number
+        number += 1
+
+    low_counter -=  1
+
+    move_idx = (move_idx + 1)  % 4
+
+
+for row in grp:
+    for num in row:
+        print(num, end=" ")
+    print()
