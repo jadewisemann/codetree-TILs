3b8c0e4ee60592a058b99618141da4b9e80f0453
jadewisemann <139527412+jadewisemann@users.noreply.github.com>
2024-07-28 10:15:07 +0900

    Title: 체크판위에서 2, Time: 184ms, Memory: 28MB, Status: Passed - CodeTree

--- 240728/체크판위에서 2/on-the-checkboard-2.py
+++ 240728/체크판위에서 2/on-the-checkboard-2.py
@@ -1,24 +1,19 @@
 def sol():
-    R, C = tuple(map(int, input().split()))
-    grp = [list(map(str, list(input().split()))) for _ in range( R )]
-
-    starting, stopovers, counter  = grp[0][0], [ ], 0
-
-    if grp[0][0] == grp[R-1][C-1]:
-        return counter
-
-    for idx in range( R-1 ):
-        for jdx in range( C-1 ):
-            if grp[idx][jdx] != starting:
-                stopovers.append([idx, jdx])
-
-
-    for xx, yy in stopovers:
-        for idx in range(xx+1, R-1):
-            for jdx in range(yy+1, C-1):
-                if grp[idx][jdx] == starting:
-                    counter += 1
-
-    return counter 
+    R, C = map(int, input().split())
+    grid = [input().split() for _ in range(R)]
+    
+    def count_paths(x1, y1, x2, y2):
+        if grid[x1][y1] == grid[x2][y2]:
+            return 0
+        return 1
+    
+    total = 0
+    for i in range(1, R-1):
+        for j in range(1, C-1):
+            for k in range(i+1, R-1):
+                for l in range(j+1, C-1):
+                    total += count_paths(0, 0, i, j) * count_paths(i, j, k, l) * count_paths(k, l, R-1, C-1)
+    
+    return total
 
 print(sol())
